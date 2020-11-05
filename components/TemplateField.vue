37b2bdb011634c2b0af96b0f5d76c2832b62af6c
Bernhard Wittmann <dev@bernhardwittmann.com>
2020-11-05 09:59:14 +0100

    feat(Sidebar): Added Drag And Drop Styling for Template Fields

--- components/TemplateField.vue
+++ components/TemplateField.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="template-field">
+  <div :class="classes" draggable="true" @dragstart="startDrag" @dragend="endDrag">
     <font-awesome-icon v-if="fieldConfig.icon" :icon="fieldConfig.icon" class="template-field__icon"></font-awesome-icon>
     <div v-if="fieldConfig.text" class="template-field__illustration-string" :style="'font-size: ' + fieldConfig.textSize">{{ fieldConfig.text }}</div>
     <div class="template-field__text">{{ fieldConfig.title }}</div>
@@ -43,9 +43,40 @@ const config: Record<FieldTypes, FieldConfig> = {
 @Component
 export default class TemplateField extends Vue {
   @Prop(EnumProp(FieldTypes.SHORT_TEXT, FieldTypes)) readonly type!: FieldTypes
+  private isBeingDragged: boolean = false
 
   get fieldConfig(): FieldConfig {
     return config[this.type]
+  }
+
+  get classes(): Record<string, boolean> {
+    return {
+      'template-field': true,
+      'template-field--dragging': this.isBeingDragged
+    }
+  }
+
+  startDrag(event: DragEvent): void {
+    event.dataTransfer!.setData('type', this.type)
+    try {
+      const el = this.$el.cloneNode(true) as HTMLElement
+      el.classList.add('template-field--dragged')
+      el.id = "drag-ghost-image"
+      el.style.position = "absolute";
+      el.style.top = "-1000px";
+      document.body.appendChild(el)
+      event.dataTransfer!.setDragImage(el, 0, 15)
+      this.isBeingDragged = true
+    } catch (e) {
+      console.warn("Your browser does not support setting a drag image", e)
+    }
+  }
+  endDrag(): void {
+    this.isBeingDragged = false
+    const ghost = document.getElementById("drag-ghost-image");
+    if (ghost && ghost.parentNode) {
+      ghost.parentNode.removeChild(ghost);
+    }
   }
 }
 </script>
@@ -62,10 +93,24 @@ export default class TemplateField extends Vue {
   align-items: center;
   margin: 8px;
   padding: 10px;
-  transition: background-color 200ms ease-in-out;
+  transition: all 100ms ease-in-out;
+  border: none;
   cursor: pointer;
-  &:hover {
-    background-color: darken(white, 20%);
+  box-sizing: border-box;
+  &--dragging {
+    border: 2px dashed rgba(#646e6e, 0.5);
+    background-color: transparent;
+    & > * {
+      display: none;
+    }
+  }
+  &--dragged {
+    border: none;
+    background-color: white;
+    transform: rotate(-5deg);
+    & > * {
+      display: block;
+    }
   }
 
   &__icon {
